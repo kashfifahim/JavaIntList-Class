9063ea39b537ae0a679f9a6e0b660da2ca767e87
Kashfi Fahim <kashfi.fahim@gmail.com>
2024-02-07 02:22:00 -0500

    Up to 2-1 of Hug's book

--- IntList.java
+++ IntList.java
@@ -6,4 +6,30 @@ public class IntList {
         this.first = first;
         this.rest = rest;
     }
+
+    /* Return the size of the list using recursion */
+    public int size() {
+        // Base case
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Size of the list without recursion */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    /** TO DO */
+    public int get(int ithItem) {
+        return 0;
+
+    }
 }
