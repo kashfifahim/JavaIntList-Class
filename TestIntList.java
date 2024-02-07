eb92975ae0df6a3b97899915d71841335b32700c
Kashfi Fahim <kashfi.fahim@gmail.com>
2024-02-07 02:59:40 -0500

    End of day

--- TestIntList.java
+++ TestIntList.java
@@ -1,7 +1,12 @@
 public class TestIntList {
     public static void main(String[] args) {
-        IntList L = new IntList(5, null);
-        L.rest = new IntList(10, null);
-        L.rest.rest = new IntList(15, null);
+        // IntList L = new IntList(5, null);
+        // L.rest = new IntList(10, null);
+        // L.rest.rest = new IntList(15, null);
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(15);
+        int x = L.getFirst();
+        System.out.println(x);
     }
 }
